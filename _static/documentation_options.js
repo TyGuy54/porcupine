8872ac2e20b536b8e92d1ba536304f4d83ba53c0
Akuli <akuviljanen17@gmail.com>
2021-01-18 14:31:41 +0200

    updating docs with docs/publish.py

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.81.1+git.440018f',
+    VERSION: '0.82.0+git.b4bd1be',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
